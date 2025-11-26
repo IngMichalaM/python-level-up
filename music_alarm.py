86af4e4c55ac82a90fd56bb4d77b426a6d592ef3
IngMichalaM <cadova.michala@gmail.com>
2025-11-26 08:31:04 +0100

    music alarm, official solution

---
+++ music_alarm.py
@@ -0,0 +1,13 @@
+# At a defined time play the defined music file
+
+import time
+import sched
+
+# Official solution
+def schedule_function(event_time, function, *args):
+    s = sched.scheduler(time.time, time.sleep)
+    s.enterabs(event_time, 1, function, argument=args)
+    print(f'{function.__name__} scheduled for {time.asctime(time.localtime(event_time))}.')
+    s.run()
+
+schedule_function(time.time()+15, print, 'This is your scheduled alarm message!')
